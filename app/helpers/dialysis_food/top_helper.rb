f657c0f7753c06468086bcc2f020a41752b1af63
yoshi <yoshi@yoshinoMacBook-Pro.local>
2021-11-28 15:34:40 +0900

    topコントローラー実装

---
+++ app/helpers/dialysis_food/top_helper.rb
@@ -0,0 +1,2 @@
+module DialysisFood::TopHelper
+end
---
+++ test/controllers/dialysis_food/top_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class DialysisFood::TopControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
